8d3be3a918a231bea5d1ea45eae11cf7cc75574e
padmaja01 <padmajach001@gmail.com>
2022-01-23 23:37:13 +0530

    commit MagicSquare

--- src/MagicSquare.java
+++ src/MagicSquare.java
@@ -4,6 +4,7 @@ public class MagicSquare {
     public static void main(String[] args)
     {   Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        int flag=0;
         int[][] arr=new int[n][n];
         for(int i=0;i<n;i++)
         {
@@ -12,13 +13,34 @@ public class MagicSquare {
                 arr[i][j]=sc.nextInt();
             }
         }
+
         int sumOfForwardDiagonal=0,sumOfBackwardDiagonal=0;
+
         for(int i=0;i<n;i++)
         {
             sumOfForwardDiagonal+=arr[i][i];
             sumOfBackwardDiagonal+=arr[i][n-1-i];
         }
         if(sumOfForwardDiagonal==sumOfBackwardDiagonal)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int sumOfEachRow = 0, sumOfEachColumn = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sumOfEachRow += arr[i][j];
+                    sumOfEachColumn += arr[j][i];
+                }
+                if (sumOfEachColumn != sumOfEachRow || sumOfEachRow != sumOfForwardDiagonal)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        else
+            flag=1;
+        if(flag==0)
             System.out.println("Yes");
         else
             System.out.println("No");
